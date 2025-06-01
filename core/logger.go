9415e247f43136c5cd270c64d6c27c05baeddd2e
LizaMeytner <danila.naumov.4444@bk.ru>
2025-06-02 01:27:27 +0400

    Add files via upload

---
+++ core/logger.go
@@ -0,0 +1,11 @@
+package core
+
+//Интерфейс
+type Logger interface {
+	Debug(msg string, keysAndValues ...interface{})
+	Info(msg string, keysAndValues ...interface{})
+	Warn(msg string, keysAndValues ...interface{})
+	Error(msg string, keysAndValues ...interface{})
+	Fatal(msg string, keysAndValues ...interface{})
+	With(keysAndValues ...interface{}) Logger
+}
